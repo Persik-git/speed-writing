768c00230295f908564c43319c767804389b3562
Persik-git <timurzelkovskij@gmail.com>
2025-03-01 11:45:04 +0300

    Update script.js
    Первый набросок js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let test_mistakes = document.querySelector('.mistakes')
+let test_time = document.querySelector('.time')
+let test_result = document.querySelector('.test-result')
+let start_test = document.querySelector('.start-button')
+let test_result_letters = document.querySelector('.letters')
+
+function start_test() {
+
+}
 
