d6f22f13eafb22907791d163b9221c24ffee2ce0
Persik-git <timurzelkovskij@gmail.com>
2025-03-22 12:21:32 +0300

    Update script.js
    1.2

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-let timeLeft = 60;
-let timer;
-let timerDisplay = document.getElementById("timer-display"); 
-let textContainer = document.getElementById("text-container"); 
-let startButton = document.querySelector(".start-button");
+let table = document.querySelector('.styled-table')
+let startButton = table.querySelector('.start-button')
+let timer = table.querySelector('.timer')
 
-function startTimer() {
-    timeLeft = 60;
-    timerDisplay.textContent = timeLeft; 
-    clearInterval(timer);
+let textContainer = document.querySelector('.text-container')
+let text = textContainer.querySelector('p').innerHTML
 
-    textContainer.style.display = "block"; // Показываем текст после нажатия кнопки
+//let textTrue = new Array()
+//text.split()
+//console.log(textTrue.length)
 
-    timer = setInterval(() => {
-        timeLeft--;
-        timerDisplay.textContent = timeLeft;
+startButton.addEventListener('click', function(){
+    timer.style.display = 'flex'
+    textContainer.style.display = 'flex'
+    startCountdown()
+    startTimer()
+})
 
-        if (timeLeft <= 0) {
-            clearInterval(timer);
-            alert("Время вышло!");
-        }
-    }, 1000);
+// Функция для начала отсчета
+function startCountdown() {
+    timer.innerHTML = "Успей подготовиться!";
+    // Запускаем обратный отсчет на 3 секунды
+    setTimeout(() => {
+        timer.textContent = ""; // Очищаем сообщение
+    }, 30000); // Задержка в 3000 миллисекунд (3 секунды)
+    startTimer(); // Запускаем таймер на 60 секунд
 }
 
-startButton.addEventListener("click", startTimer);
+// Функция для 60-секундного таймера
+function startTimer() {
+    let seconds = 60; 
+    const interval = setInterval(() => {
+        timer.textContent = "Осталось "+ seconds+" секунд"; // Обновляем отображение времени
+        seconds--; // Уменьшаем количество оставшихся секунд
+        // Если время закончилось, останавливаем таймер и показываем сообщение
+        if (seconds < 0) {
+            clearInterval(interval); // Останавливаем интервал
+            timer.textContent = "Ты не успел!"; // Показываем сообщение о том, что время истекло
+        }
+    }, 1000); // Обновляем каждую секунду
+}
